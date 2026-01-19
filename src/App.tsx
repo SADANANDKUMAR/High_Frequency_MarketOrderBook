7890e01ac40a2e99f52dad479ca93d1857dff576
Sadanand kumar <sadanandkr9686@gmail.com>
2026-01-19 11:56:35 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+// import viteLogo from '/vite.svg';
+// import {useState} from 'react';
+
+// import reactLogo from './assets/react.svg';
+
+function App() {
+    return (
+        <>
+            <h1 className='text-3xl font-bold text-blue-600'>
+                Tailwind Working
+            </h1>
+        </>
+    );
+}
+
+export default App;
